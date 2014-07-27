4867416b1853b9178249954fc7a78d2533d8a122
Derick Beckwith <derick.beckwith@gmail.com>
2014-07-26 22:00:58 -0400

    Refactored out duplicated code into a SwapFragment method

--- src/com/teampo8/po8minihack/MainActivity.java
+++ src/com/teampo8/po8minihack/MainActivity.java
@@ -7,7 +7,6 @@ import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentTransaction;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -16,92 +15,69 @@ import android.view.View;
 import android.view.ViewGroup;
 
 public class MainActivity extends Activity implements TabListener {
-
-	private final String TAG = "MainActivity";
-
 	// detects swipes
-	float x1, x2;
-	float y1, y2;
+	float x1, x2, y1, y2;
 
 	// fragments
 	FragmentTransaction transaction = null;
-	PlaceHolderFragment mainFragment = new PlaceHolderFragment(R.layout.fragment_main);
-	PlaceHolderFragment rightFragment = new PlaceHolderFragment(R.layout.fragment_right);
-	PlaceHolderFragment leftFragment = new PlaceHolderFragment(R.layout.fragmant_left);
+	PlaceHolderFragment mainFragment = new PlaceHolderFragment(
+			R.layout.fragment_main);
+	PlaceHolderFragment rightFragment = new PlaceHolderFragment(
+			R.layout.fragment_right);
+	PlaceHolderFragment leftFragment = new PlaceHolderFragment(
+			R.layout.fragmant_left);
 
 	final int FRAGMENT_LEFT = 0;
 	final int FRAGMENT_MAIN = 1;
 	final int FRAGMENT_RIGHT = 2;
-	int position = FRAGMENT_MAIN; // position of current fragment
+	int position = FRAGMENT_LEFT; // position of current fragment
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
+
 		// setup tabs in the action bar
 		ActionBar bar = getActionBar();
 		bar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
-		
-		Tab tab1 = bar.newTab();
-		tab1.setText("Tab One");
-		tab1.setTabListener(this);
-		bar.addTab(tab1);
-		
-		Tab tab2 = bar.newTab();
-		tab2.setText("Tab Two");
-		tab2.setTabListener(this);
-		bar.addTab(tab2);
-		
-		Tab tab3 = bar.newTab();
-		tab3.setText("Tab Three");
-		tab3.setTabListener(this);
-		bar.addTab(tab3);
-		
+
+		for (int i = 0; i < 3; i++) {
+			Tab tab = bar.newTab();
+			tab.setText("Tab " + (i + 1));
+			tab.setTabListener(this);
+			bar.addTab(tab);
+		}
+
 		// set main fragment
 		transaction = getFragmentManager().beginTransaction();
-		transaction.replace(R.id.container, mainFragment);
+		transaction.replace(R.id.container, leftFragment);
 		transaction.commit();
 	}
-	
+
 	@Override
 	public void onTabSelected(Tab tab, FragmentTransaction transaction) {
-		Log.d(TAG, "onTabSelected");
 		if (transaction.isEmpty()) {
-			Log.d(TAG, "Frag transaction is empty");
-			if (tab.getText() == "Tab One") {
-				Log.d(TAG, "Tab One Selected");
+			if (tab.getPosition() == FRAGMENT_LEFT) {
 				position = FRAGMENT_LEFT;
-				transaction = getFragmentManager().beginTransaction();
-				transaction.replace(R.id.container, leftFragment);
-				transaction.addToBackStack(null);
-				transaction.commit();
-			} else if (tab.getText() == "Tab Two") {
-				Log.d(TAG, "Tab Two Selected");
+				SwapFragment(leftFragment, transaction);
+			} else if (tab.getPosition() == FRAGMENT_MAIN) {
 				position = FRAGMENT_MAIN;
-				transaction = getFragmentManager().beginTransaction();
-				transaction.replace(R.id.container, mainFragment);
-				transaction.addToBackStack(null);
-				transaction.commit();
-			} else if (tab.getText() == "Tab Three") {
-				Log.d(TAG, "Tab Three Selected");
+				SwapFragment(mainFragment, transaction);
+			} else if (tab.getPosition() == FRAGMENT_RIGHT) {
 				position = FRAGMENT_RIGHT;
-				transaction = getFragmentManager().beginTransaction();
-				transaction.replace(R.id.container, rightFragment);
-				transaction.addToBackStack(null);
-				transaction.commit();
+				SwapFragment(rightFragment, transaction);
 			}
 		}
 	}
-	
+
 	@Override
 	public void onTabUnselected(Tab tab, FragmentTransaction transaction) {
-		
+
 	}
-	
+
 	@Override
 	public void onTabReselected(Tab tab, FragmentTransaction transaction) {
-		
+
 	}
 
 	@Override
@@ -125,78 +101,71 @@ public class MainActivity extends Activity implements TabListener {
 
 	public boolean onTouchEvent(MotionEvent touchEvent) {
 		switch (touchEvent.getAction()) {
-			// when user first touches the screen we get the x any y coordinate
-			case MotionEvent.ACTION_DOWN:
-			{
-				x1 = touchEvent.getX();
-				y1 = touchEvent.getY();
-				break;
-			}
-			case MotionEvent.ACTION_UP:
-			{
-				x2 = touchEvent.getX();
-				y2 = touchEvent.getY();
-				
-				// if right to left sweep
-				if (x1 > x2) {
-					position++;
-					if (position > 2)
-						position = 2;
-					
-					if (position == FRAGMENT_MAIN) {
-						transaction = getFragmentManager().beginTransaction();
-						transaction.replace(R.id.container, mainFragment);
-						transaction.addToBackStack(null);
-						transaction.commit();
-						getActionBar().setSelectedNavigationItem(position);
-					} else if (position == FRAGMENT_RIGHT) {
-						transaction = getFragmentManager().beginTransaction();
-						transaction.replace(R.id.container, rightFragment);
-						transaction.addToBackStack(null);
-						transaction.commit();
-						getActionBar().setSelectedNavigationItem(position);
-					}
+		// when user first touches the screen we get the x any y coordinate
+		case MotionEvent.ACTION_DOWN: {
+			x1 = touchEvent.getX();
+			y1 = touchEvent.getY();
+			break;
+		}
+		case MotionEvent.ACTION_UP: {
+			x2 = touchEvent.getX();
+			y2 = touchEvent.getY();
+
+			// if right to left sweep
+			if (x1 > x2) {
+				position++;
+				if (position > 2)
+					position = 2;
+
+				if (position == FRAGMENT_MAIN) {
+					SwapFragment(mainFragment, transaction);
+					getActionBar().setSelectedNavigationItem(position);
+				} else if (position == FRAGMENT_RIGHT) {
+					SwapFragment(rightFragment, transaction);
+					getActionBar().setSelectedNavigationItem(position);
 				}
-				
-				// if left to right sweep
-				if (x1 < x2) {
-					position--;
-					if (position < 0)
-						position = 0;
-					
-					if (position == FRAGMENT_MAIN) {
-						transaction = getFragmentManager().beginTransaction();
-						transaction.replace(R.id.container, mainFragment);
-						transaction.addToBackStack(null);
-						transaction.commit();
-						getActionBar().setSelectedNavigationItem(position);
-					} else if (position == FRAGMENT_LEFT) {
-						transaction = getFragmentManager().beginTransaction();
-						transaction.replace(R.id.container, leftFragment);
-						transaction.addToBackStack(null);
-						transaction.commit();
-						getActionBar().setSelectedNavigationItem(position);
-					}
-				}
-				break;
 			}
+
+			// if left to right sweep
+			if (x1 < x2) {
+				position--;
+				if (position < 0)
+					position = 0;
+
+				if (position == FRAGMENT_MAIN) {
+					SwapFragment(mainFragment, transaction);
+					getActionBar().setSelectedNavigationItem(position);
+				} else if (position == FRAGMENT_LEFT) {
+					SwapFragment(leftFragment, transaction);
+					getActionBar().setSelectedNavigationItem(position);
+				}
+			}
+			break;
+		}
 		}
 		return false;
 	}
-	
+
+	private void SwapFragment(Fragment fragmentToSwap,
+			FragmentTransaction transaction) {
+		transaction = getFragmentManager().beginTransaction();
+		transaction.replace(R.id.container, fragmentToSwap);
+		transaction.addToBackStack(null);
+		transaction.commit();
+	}
+
 	private final class PlaceHolderFragment extends Fragment {
-		
+
 		private final int id;
-		
+
 		public PlaceHolderFragment(int id) {
 			this.id = id;
 		}
-		
+
 		@Override
 		public View onCreateView(LayoutInflater inflater, ViewGroup container,
 				Bundle savedInstanceState) {
-			View rootView = inflater.inflate(this.id, container,
-					false);
+			View rootView = inflater.inflate(this.id, container, false);
 
 			return rootView;
 		}
